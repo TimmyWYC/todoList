ea964def7f02b87590d561f482a0f2737cdf5168
timmy <timmiw95@gmail.com>
2022-10-25 08:54:36 +0200

    Initial Commit

---
+++ MyTodoList/MyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoListApp.swift
+//  MyTodoList
+//
+//  Created by Timmy Wong on 2022-10-25.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
